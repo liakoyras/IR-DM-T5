667fc0447cdc5f1a00afb20748364410fa0ee5a2
Alex Karas <darklex55@gmail.com>
2020-04-03 22:15:18 +0300

    Update partB.py

--- Part B/partB.py
+++ Part B/partB.py
@@ -1,10 +1,21 @@
 import os
 import time
+from collections import Counter
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 def time_extraction(fun,args):
     start = time.time()
     fun(args)
     return time.time()-start
+
+def ask_Y_N(question):
+    answer = ' '
+    answers = ['Y', 'y', 'N', 'n']
+    while answer not in answers:
+        answer = raw_input(question +' (y/n): ')
+    return answer
 
 def extract_la(lines):
     doc = []
@@ -169,19 +180,21 @@ def extract_fb(lines):
 
     return doc
 
+query_file_name = raw_input('Enter the relative path/name of the query file to append (For example:\nEvaluations/IndriRunQuery...EXAMPLE): ')
+results = raw_input('Enter the relative path/name of the query results to get the words from: ')
+results = [results]
 filenames = os.listdir('Evaluations/')
-results = []
 
-for i in filenames:
-    if 'results.trec' in i:
-        results.append(i)
+
+#for i in filenames:
+#    if 'results.trec' in i:
+#        results.append(i)
         
 r_text = []
 
 for i in results:
     f = open('Evaluations/'+i, "r")
     r_text.append(f.read().splitlines())
-    
     
 pt = 301
 for k in range(len(r_text)):
@@ -198,9 +211,10 @@ for k in range(len(r_text)):
         top_15.append(split[2 + j])
         j = j + 6
         
+    os.system('rm -r Texts')
     os.system('mkdir Texts')
 
-    with open('Texts/top_15.'+results[k].split('.')[0]+'.txt', 'w') as f:
+    with open('top_15.titles-only.txt', 'w') as f:
         for item in top_15:
             f.write("%s\n" % item)
 
@@ -210,12 +224,12 @@ top = []
 for i in filenames:
     if 'top_15' in i:
         top.append(i)
-
+top = ['top_15.titles-only.txt']
 os.system('rm -r /Texts/sorted')
 
 a=0
-q=301
-filenum = 0
+q=0
+print('Exporting texts...')
 for i in top:
     f = open('Texts/'+i, "r")
     fr = []
@@ -231,32 +245,36 @@ for i in top:
     for j in range(len(lines)):
         if 'FR' in lines[j]:
             fr.append(lines[j])
-            fr_ids.append(q-301)
+            fr_ids.append(q)
         if 'FT' in lines[j]:
             ft.append(lines[j])
-            ft_ids.append(q-301)
+            ft_ids.append(q)
         if 'FBIS' in lines[j]:
             fb.append(lines[j])
-            fb_ids.append(q-301)
+            fb_ids.append(q)
         if 'LA' in lines[j]:
             la.append(lines[j])
-            la_ids.append(q-301)
+            la_ids.append(q)
         a+=1
         if a==15:
             a=0
             q+=1
+    print('Exporting fr...')
     fr = extract_fr(fr)
+    print('Exporting ft...')
     ft = extract_ft(ft)
+    print('Exporting fbis...')
     fb = extract_fb(fb)
+    print('Exporting latimes...')
     la = extract_la(la)
     
     fr_id = 0
     ft_id = 0
     fb_id = 0
     la_id = 0
+    queries = []
     for j in range(150):
         current = []
-        print(j)
         
         if fr_id != len(fr_ids):
             while fr_ids[fr_id]==j:
@@ -283,9 +301,122 @@ for i in top:
                 if la_id == len(la_ids):
                     break
             
-        os.system('mkdir Texts/'+str(filenames[filenum])[:-4].replace('.','_'))
-        with open('Texts/'+str(filenames[filenum])[:-4].replace('.','_')+'/'+str(j)+'.txt', 'w') as f:
-            for item in current:
-                f.write("%s\n" % item)    
-    
-    filenum+=1
+        queries.append(current)
+
+all_words=[]
+for k in range(len(queries)):
+    c_text = ''
+    for i in queries[k]:
+        c_text = c_text + i
+    c_text = Counter(c_text.strip().split())
+    all_words.append(c_text.most_common())
+
+stopwords = set(nltk.corpus.stopwords.words('english'))
+
+c_stopwords = list(stopwords)[:]
+
+for i in range(len(c_stopwords)):
+    c_stopwords[i] = c_stopwords[i][0].upper()+c_stopwords[i][1:]
+
+c_stopwords = set(c_stopwords)
+
+final_words = []
+final_syn = []
+final_words_enriched = []
+
+for query in range(len(all_words)):
+    temp = []
+    for i in all_words[query]:
+        word = i[0]
+        tp = True
+        for k in ['<','>','/','.',',','"',"'",':',')','(',' ']:
+            word = word.replace(k,'')
+        for k in ['DOCID','CELLRULE','RULETABLE','ITAG','newline','TMJ','PJG','=','#','&','*','+','QTAG','PPQ','PPThe','0','1','2','3','4','5','6','7','8','9']:
+            if k in word:
+                tp=False
+                break
+        if word not in stopwords and word not in c_stopwords and len(word)>2 and tp:
+            temp.append(word)
+        if len(temp)==20:
+            break
+
+    f = []
+    for i in range(len(temp)):
+        synonyms = []
+        for syn in nltk.corpus.wordnet.synsets(temp[i]):
+            for l in syn.lemmas():
+                synonyms.append(l.name())
+        synonyms.append(temp[i])
+        synonyms = list(set(synonyms))
+        f = f + synonyms
+
+    t = list(set(f))
+    f = ''
+    for i in t:
+        f = f + ' ' + i
+    final_words_enriched.append(f)
+
+    f = ''
+    for i in temp:
+        f = f+ ' '+i
+    final_words.append(f)
+
+    f = []
+    for i in range(len(temp)):
+        synonyms = []
+        for syn in nltk.corpus.wordnet.synsets(temp[i]):
+            for l in syn.lemmas():
+                synonyms.append(l.name())
+        synonyms = list(set(synonyms))
+        f = f + synonyms
+        
+    t = list(set(f))
+    f = ''
+    for i in t:
+        f = f + ' ' + i
+    final_syn.append(f)
+
+f = open('IndriRunQuery.queries.file.301-450-titles.EXAMPLE', "r")
+query_file_name='IndriRunQuery.queries.file.301-450-titles.EXAMPLE'
+query_file=f.read().splitlines()
+
+
+ftemp = []
+for i in query_file:
+    ftemp.append(i)
+    if '<rule>' in i:
+        ftemp.append('<retModel>indri</retModel> <fbMu>0.0</fbMu> <fbOrigWeight>0.5</fbOrigWeight>')
+
+query_file_syns_only = query_file[:]
+query_file = ftemp[:]
+query_file_syns = ftemp[:]
+
+query_num = 0
+for i in range(len(query_file)):
+    if '<query>' in query_file[i]:
+        query_file[i] = query_file[i][:query_file[i].index('<text>')+6]+query_file[i][query_file[i].index('<text>')+6:query_file[i].index('</text>')] + final_words[query_num] +  query_file[i][query_file[i].index('</text>'):]
+        query_num+=1
+        
+query_num = 0
+for i in range(len(query_file_syns)):
+    if '<query>' in query_file_syns[i]:
+        query_file_syns[i] = query_file_syns[i][:query_file_syns[i].index('<text>')+6]+query_file_syns[i][query_file_syns[i].index('<text>')+6:query_file_syns[i].index('</text>')] + final_words_enriched[query_num] +  query_file_syns[i][query_file_syns[i].index('</text>'):]
+        query_num+=1
+        
+query_num = 0
+for i in range(len(query_file_syns_only)):
+    if '<query>' in query_file_syns_only[i]:
+        query_file_syns_only[i] = query_file_syns_only[i][:query_file_syns_only[i].index('<text>')+6]+query_file_syns_only[i][query_file_syns_only[i].index('<text>')+6:query_file_syns_only[i].index('</text>')] + final_syn[query_num] +  query_file_syns_only[i][query_file_syns_only[i].index('</text>'):]
+        query_num+=1        
+
+with open(query_file_name.split('/')[-1].replace('.ERGASIA','-RF.ERGASIA').replace('.EXAMPLE','-RF.EXAMPLE'), 'w') as f:
+    for item in query_file:
+        f.write("%s\n" % item)
+
+with open(query_file_name.split('/')[-1].replace('.ERGASIA','-RF-SYN.ERGASIA').replace('.EXAMPLE','RF-SYN.EXAMPLE'), 'w') as f:
+    for item in query_file_syns:
+        f.write("%s\n" % item)
+        
+with open(query_file_name.split('/')[-1].replace('.ERGASIA','-ONLY-SYN.ERGASIA').replace('.EXAMPLE','-ONLY-SYN.EXAMPLE'), 'w') as f:
+    for item in query_file_syns_only:
+        f.write("%s\n" % item)
